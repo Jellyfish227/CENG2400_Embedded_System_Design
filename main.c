5b7bdbb691ca5812aa5b0015bf5fbcc8ba190d3c
Jellyfish_Yu <jellyfishyu0227@gmail.com>
2024-09-24 15:35:51 +0800

    updated lab3

--- main.c
+++ main.c
@@ -1,118 +1,171 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
+#include "inc/tm4c123gh6pm.h"
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
+#include "inc/hw_gpio.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
-#include "inc/hw_gpio.h"
-volatile int32_t ButtonState = 0;
-volatile uint32_t g_w_delay = 2000000;
-volatile uint32_t g_RGB_delay = 20000000;
+#include "driverlib/timer.h"
+#include "driverlib/interrupt.h"
+
+#define W_DELAY 150
+#define RGB_DELAY 1500
+
 volatile uint8_t g_flash_LED = 0;
-volatile uint8_y timer0finish = 1;
+bool timer1finish = 1;
+bool timer0finish = 1;
 
-void PORT4_IRQHandler(void) {
+uint32_t ui32Period;
 
+void Timer1IntHandler(void);
+void Timer0IntHandler(void);
+
+void initialization(void)
+{
+    SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4 | GPIO_PIN_0);
+    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+
+    // Configure Timer1
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER1);
+    TimerConfigure(TIMER1_BASE, TIMER_CFG_ONE_SHOT);
+    IntEnable(INT_TIMER1A);
+    TimerIntRegister(TIMER1_BASE, TIMER_A, Timer1IntHandler);
+    TimerIntEnable(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
+
+    ui32Period = SysCtlClockGet() / 1000;
+    TimerLoadSet(TIMER1_BASE, TIMER_A, RGB_DELAY * ui32Period);
+
+    // Configure Timer0
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+    TimerConfigure(TIMER0_BASE, TIMER_CFG_ONE_SHOT);
+    IntEnable(INT_TIMER0A);
+    TimerIntRegister(TIMER0_BASE, TIMER_A, Timer0IntHandler);
+    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+
+    TimerLoadSet(TIMER0_BASE, TIMER_A, W_DELAY * ui32Period);
+
+    IntMasterEnable();
 }
 
-void read_Switches(void) {
-    if (GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4)) {
+// TODO: Using GPIO Interrupt to replace the function Read_Switches_Timer()
+/* void Read_Switches_Timer(void)
+{
+    if (GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4))
         g_flash_LED = 0;
-    } else {
+    else
         g_flash_LED = 1;
+} */
+
+void PORTD_IRQHandler(void) {
+    if (PORTD->ISFR && MASK(GPIO_PIN_4)) {
+        if (GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4))
+            g_flash_LED = 0;
+        else
+            g_flash_LED = 1;
     }
+    // Clear the interrupt flag
+    PORTD->ISFR = 0xffffffff;
 }
 
-void timer0IntHandler(void) {
-    TimerIntClear(timer0_BASE, TIMER_TIMA_TIMEOUT);
+void Timer1IntHandler(void)
+{
+    TimerIntClear(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
+    timer1finish = 1;
+}
+
+void Timer0IntHandler(void)
+{
+    TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
     timer0finish = 1;
 }
 
-void RGB_FSM(void) {
-    static enum {
-        ST_RED, ST_GREEN, ST_BLUE, 
-        ST_RED_WAIT, ST_GREEN_WAIT, ST_BLUE_WAIT, ST_OFF} next_state;
-    if (g_flash_LED == 0) {
-        switch (next_state) {
-            case ST_RED:
-                //Control_RGB_LEDs(1, 0, 0);
-                GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_1);
-                // TODO: Start timer
-                timer0finish = 0;
-                TimerEnable(timer0_BASE, TIMER_A)
-                SysCtlDelay(g_RGB_delay);
-                next_state = ST_RED_WAIT;
-                break;
-            case ST_RED_WAIT:
-                if (/* timeout */timer0finish) {
-
-                }
-                break;
-            case ST_BLUE:
-                //Control_RGB_LEDs(0, 1, 0);
-                GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_2);
-                SysCtlDelay(g_RGB_delay);
-                next_state = ST_BLUE_WAIT;
-                break;
-            case ST_GREEN:
-                //Control_RGB_LEDs(0, 0, 1);
-                GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_3);
-                SysCtlDelay(g_RGB_delay);
-                next_state = ST_GREEN_WAIT;
-                break;
-
-            default:
-                next_state = ST_RED;
-                break;
-        }
-    }
-}
-
-void flash_FSM(void) {
-    static enum {ST_WHITE, ST_BLACK} next_state;
+void Flash_Timer(void)
+{
+    // TODO: Implement this function similar to RGB_Timer()
+    static enum {W, W_wait} next_state;
     if (g_flash_LED == 1) {
+        // GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, GPIO_PIN_1);
+        // SysCtlDelay(W_DELAY);
+        // GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0);
+        // SysCtlDelay(W_DELAY);
         switch (next_state) {
-            case ST_WHITE:
-                //Control_RGB_LEDs(1, 1, 1);
-                GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
-                SysCtlDelay(g_w_delay);
-                next_state = ST_BLACK;
+            case W:
+                GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, GPIO_PIN_1);
+                TimerEnable(TIMER0_BASE, TIMER_A);
+                next_state = W_wait;
                 break;
-            case ST_BLACK:
-                //Control_RGB_LEDs(0, 0, 0);
-                GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x00);
-                SysCtlDelay(g_w_delay);
-                next_state = ST_WHITE;
+            case W_wait:
+                if (timer0finish)
+                    next_state = W;
                 break;
             default:
-                next_state = ST_WHITE;
+                next_state = W;
                 break;
         }
     }
 }
 
-void flash(void) {
-    read_Switches();
-    flash_FSM();
-    RGB_FSM();
+void RGB_Timer(void)
+{
+    if (g_flash_LED == 0)
+    {
+        static enum { R,
+                      R_wait,
+                      G,
+                      G_wait,
+                      B,
+                      B_wait } next_state;
+        switch (next_state)
+        {
+        case R:
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, GPIO_PIN_1);
+            timer1finish = 0;
+            TimerEnable(TIMER1_BASE, TIMER_A);
+            next_state = R_wait;
+            break;
+        case R_wait:
+            if (timer1finish)
+                next_state = G;
+            break;
+        case G:
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, GPIO_PIN_2);
+            timer1finish = 0;
 
-}
-
-voide Initialzation(void) {
-
+            TimerEnable(TIMER1_BASE, TIMER_A);
+            next_state = G_wait;
+            break;
+        case G_wait:
+            if (timer1finish)
+                next_state = B;
+            break;
+        case B:
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, GPIO_PIN_3);
+            timer1finish = 0;
+            TimerEnable(TIMER1_BASE, TIMER_A);
+            next_state = B_wait;
+            break;
+        case B_wait:
+            if (timer1finish)
+                next_state = R;
+            break;
+        default:
+            next_state = R;
+            break;
+        }
+    }
 }
 
 int main(void)
 {
-    SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
-       SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-       GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
-       //We use pin4 to control SW1
-       GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4);
-       //Since the button needs some sort of pull-up, we set pin4 as weak pull-up
-       GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
-
-       while (1) {
-           flash();
-       }
+    initialization();
+    while (1)
+    {
+        RGB_Timer();
+        Flash_Timer();
+    }
 }
