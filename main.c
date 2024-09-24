4e7967c87c4aa61c4c5503645671310488239ca8
Jellyfish_Yu <jellyfishyu0227@gmail.com>
2024-09-24 13:33:15 +0800

    init commit

--- main.c
+++ main.c
@@ -5,10 +5,15 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
 #include "inc/hw_gpio.h"
-int32_t ButtonState = 0;
-uint32_t g_w_delay = 2000000;
-uint32_t g_RGB_delay = 20000000;
-uint8_t g_flash_LED = 0;
+volatile int32_t ButtonState = 0;
+volatile uint32_t g_w_delay = 2000000;
+volatile uint32_t g_RGB_delay = 20000000;
+volatile uint8_t g_flash_LED = 0;
+volatile uint8_y timer0finish = 1;
+
+void PORT4_IRQHandler(void) {
+
+}
 
 void read_Switches(void) {
     if (GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4)) {
@@ -18,27 +23,42 @@ void read_Switches(void) {
     }
 }
 
+void timer0IntHandler(void) {
+    TimerIntClear(timer0_BASE, TIMER_TIMA_TIMEOUT);
+    timer0finish = 1;
+}
+
 void RGB_FSM(void) {
-    static enum {ST_RED, ST_GREEN, ST_BLUE, ST_OFF} next_state;
+    static enum {
+        ST_RED, ST_GREEN, ST_BLUE, 
+        ST_RED_WAIT, ST_GREEN_WAIT, ST_BLUE_WAIT, ST_OFF} next_state;
     if (g_flash_LED == 0) {
         switch (next_state) {
             case ST_RED:
                 //Control_RGB_LEDs(1, 0, 0);
                 GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_1);
+                // TODO: Start timer
+                timer0finish = 0;
+                TimerEnable(timer0_BASE, TIMER_A)
                 SysCtlDelay(g_RGB_delay);
-                next_state = ST_BLUE;
+                next_state = ST_RED_WAIT;
+                break;
+            case ST_RED_WAIT:
+                if (/* timeout */timer0finish) {
+
+                }
                 break;
             case ST_BLUE:
                 //Control_RGB_LEDs(0, 1, 0);
                 GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_2);
                 SysCtlDelay(g_RGB_delay);
-                next_state = ST_GREEN;
+                next_state = ST_BLUE_WAIT;
                 break;
             case ST_GREEN:
                 //Control_RGB_LEDs(0, 0, 1);
                 GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_3);
                 SysCtlDelay(g_RGB_delay);
-                next_state = ST_RED;
+                next_state = ST_GREEN_WAIT;
                 break;
 
             default:
@@ -75,6 +95,10 @@ void flash(void) {
     read_Switches();
     flash_FSM();
     RGB_FSM();
+
+}
+
+voide Initialzation(void) {
 
 }
 
