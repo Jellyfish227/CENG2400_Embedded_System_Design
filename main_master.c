685da9d0b736cefcb6efd5d51555b173c11451aa
C. H. Yu <jellyfishyu0227@gmail.com>
2024-10-22 09:31:20 +0000

    finished master

--- main_master.c
+++ main_master.c
@@ -30,6 +30,11 @@ Steps:
 #include "driverlib/sysctl.h"
 #include "driverlib/uart.h"
 #include "utils/uartstdio.h"
+#include "inc/hw_ints.h"
+#include "driverlib/interrupt.h"
+
+
+void PORTF_IRQHandler(void);
 
 int main(void) {
     // set clock
@@ -37,9 +42,16 @@ int main(void) {
 
     // TODO: YOUR UART INITIALIZATION PROCEDURE
     // enable UART5 and GPIOE to communicate with BLUETOOTH
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART5);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
     // configure PE4 for RX, PE5 for TX
+    GPIOPinConfigure(GPIO_PE4_U5RX);
+    GPIOPinConfigure(GPIO_PE5_U5TX);
     // set PORTE pin4 and pin5 as type UART
+    GPIOPinTypeUART(GPIO_PORTE_BASE, GPIO_PIN_4 | GPIO_PIN_5);
     // set UART5 base address, clock and baud rate
+    UARTConfigSetExpClk(UART5_BASE, SysCtlClockGet(), 38400,
+        (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
 
     // enable SW1 of GPIOF for button control
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
@@ -50,4 +62,26 @@ int main(void) {
     // you can do
     // polling (i.e., checking button status and send data in a while loop), or
     // interrupt (i.e., writing an interrupt function to check button status and send data)
+    GPIOIntTypeSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_BOTH_EDGES);
+    GPIOIntEnable(GPIO_PORTF_BASE, GPIO_PIN_4);
+    GPIOIntRegister(GPIO_PORTF_BASE, PORTF_IRQHandler);
+    IntMasterEnable();
+}
+
+void PORTF_IRQHandler(void)
+{
+    // Check if the interrupt was triggered by PD4
+    if (GPIOIntStatus(GPIO_PORTF_BASE, true) & GPIO_INT_PIN_4)
+    {
+        char a;
+        // Toggle the LED or perform your action
+        if (GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4))
+            a = 'Y';
+        else
+            a = 'X'; // Button pressed
+
+        UARTCharPut(UART5_BASE, a);
+    }
+    // Clear the interrupt flag
+    GPIOIntClear(GPIO_PORTF_BASE, GPIO_INT_PIN_4);
 }
