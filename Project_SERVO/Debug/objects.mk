b5d3c9c3637293d72fa8b5f97f0865f5422b922b
Ching Hei YU <1155193237@link.cuhk.edu.hk>
2024-12-20 02:57:15 +0800

    Remove obsolete files from Project_MPU and Project_SERVO

--- Project_SERVO/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -l"/Users/jellyfish/Documents/CUHK/Year_3/Fall/CENG2400_Embedded_System_Design/lib/TivaWare_C_Series-2.1.4.178/sensorlib/ccs/Debug/sensorlib.lib" -l"/Users/jellyfish/Documents/CUHK/Year_3/Fall/CENG2400_Embedded_System_Design/lib/TivaWare_C_Series-2.1.4.178/driverlib/ccs/Debug/driverlib.lib"
-
